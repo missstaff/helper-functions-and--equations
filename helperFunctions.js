b9d581594a683344316a70e1202a80cf96651bb3
Shawna Staff <staffs@my.lanecc.edu>
2021-08-03 22:33:05 -0700

    second commit

---
+++ helperFunctions.js
@@ -0,0 +1,43 @@
+/**
+ * // function to generate a random number between two values
+ * // uses Math.floor to handle negative numbers
+ * @param {*} min
+ * @param {*} max
+ * @returns randomInt
+ */
+const randomInt = (min, max) => Math.floor(Math.random() * (max - min) + min);
+//console.log(randomInt(10, 20));
+
+/**
+ * Checks if a number is even
+ * @param {*} n
+ * @returns bool
+ */
+const isEven = (n) => n % 2 === 0;
+
+/**
+ * Checks if a number is odd
+ * @param {*} n
+ * @returns bool
+ */
+const isOdd = (n) => n % 3 === 0;
+
+/**
+ * Display message on HTML element
+ */
+const displayMessage = (message) => {
+  document.querySelector(".className").textContent = message;
+};
+/**
+ * modular currency formatter
+ * @param {*} value
+ * @param {*} locale
+ * @param {*} currency
+ * @returns value
+ */
+const formatCur = function (value, locale, currency) {
+  return new Intl.NumberFormat(locale, {
+    style: "currency",
+    currency: currency,
+  }).format(value);
+};
---
+++ usefulEquations.js
@@ -0,0 +1,10 @@
+/**
+ * calculate average
+ */
+const calcAverage = (int1, int2, int3) => (int1 + int2 + int3) / numInts;
+
+/**
+ * // calculate radius
+ */
+//Math.PI * 10.0 **2;
+Math.PI * Number.parseFloat("10.0px") ** 2;
