6c57cd7645af13c02bc5fe1943f460e1e8d74946
Shawna Staff <staffs@my.lanecc.edu>
2021-08-23 21:37:32 -0700

    sort functions

--- helperFunctions.js
+++ helperFunctions.js
@@ -9,6 +9,36 @@ const randomInt = (min, max) => Math.floor(Math.random() * (max - min) + min);
 //console.log(randomInt(10, 20));
 
 /**
+ * @returns arr in sorted in ascending
+ */
+const sortAscending = arr.sort((a, b) => {
+  if (a > b) {
+    return 1;
+  }
+  if (b > a) {
+    return -1;
+  }
+  /** short hand arrow function
+   * arr.sort((a,b) => a - b);
+   */
+});
+
+/**
+ * @returns arr in sorted in descending order
+ */
+const sortDescending = arr.sort((a, b) => {
+  if (a < b) {
+    return 1;
+  }
+  if (b < a) {
+    return -1;
+  }
+  /** short hand arrow function
+   * arr.sort((a,b) => a + b);
+   */
+});
+
+/**
  * Checks if a number is even
  * @param {*} n
  * @returns bool
